49043dd09b84f2ced4a1012580563c442d3446b0
Samir <samir@example.com>
2025-08-07 14:13:54 +0300

    add adaptive

--- src/widgets/AboutMe/ui/AboutMe.tsx
+++ src/widgets/AboutMe/ui/AboutMe.tsx
@@ -8,25 +8,33 @@ const eventsMeta = ["2019", "2020", "2021", "2022", "2024"];
 const AboutMe = () => {
   const { aboutMeRef } = useRefsContext();
 
-  const events = eventsMeta.map((event, index) => (
-    <Fragment key={index}>
-      <p className="text-white text-[2.25rem] self-end">{event}</p>
-      <div className="w-[40px] h-[40px] rounded-[50%] bg-white"></div>
-      <p className="text-white text-[1.75rem] self-start">
-        <FormattedMessage id={`landing_event${event}`} />
-      </p>
-    </Fragment>
-  ));
+  const slicedEvents =
+    window.innerWidth < 600
+      ? [eventsMeta.slice(0, 3), eventsMeta.slice(3)]
+      : [eventsMeta];
 
   return (
     <section className="px-[10%] flex flex-col items-center" ref={aboutMeRef}>
       <H2Title>
         <FormattedMessage id="landing_aboutMe" />
       </H2Title>
-      <div className="grid grid-rows-3 grid-flow-col place-items-center relative auto-cols-fr w-full">
-        <div className="absolute w-[90%] bg-white h-3 rounded-md"></div>
-        {events}
-      </div>
+      {slicedEvents.map((events, index) => (
+        <div
+          key={index}
+          className="grid grid-rows-3 grid-flow-col place-items-center relative auto-cols-fr w-full"
+        >
+          <div className="absolute w-[90%] bg-white h-3 rounded-md"></div>
+          {events.map((event, index) => (
+            <Fragment key={index}>
+              <p className="text-white text-[2.25rem] self-end">{event}</p>
+              <div className="w-[40px] h-[40px] rounded-[50%] bg-white"></div>
+              <p className="text-white text-[1.75rem] self-start">
+                <FormattedMessage id={`landing_event${event}`} />
+              </p>
+            </Fragment>
+          ))}
+        </div>
+      ))}
     </section>
   );
 };
--- src/widgets/Begin/ui/Begin.tsx
+++ src/widgets/Begin/ui/Begin.tsx
@@ -14,12 +14,12 @@ export const Begin = () => {
   return (
     <section className="box-border px-[10%] pt-24 flex flex-col gap-6 items-center relative">
       <img className="absolute right-0 top-3 max-w-[600px]" src={drawing} />
-      <h1 className="text-white text-[4rem] font-bold">
+      <h1 className="text-white text-[4rem] font-bold text-center">
         <FormattedMessage id="landing_header" />
       </h1>
-      <div className="flex gap-[40px] align-center">
-        <img src={me} className="rounded-[1em]" style={{ maxWidth: 400 }} />
-        <p className="text-[#BCBCBC] text-[2.24rem] font-medium w-1/2">
+      <div className="flex gap-[40px] items-center md:items-start flex-col md:flex-row">
+        <img src={me} className="rounded-[1em] w-2/3 md:w-1/2" />
+        <p className="text-[#BCBCBC] text-[2.24rem] font-medium w-full md:w-1/2">
           <FormattedMessage id="landing_description" />
         </p>
       </div>
