8b03bddad5a7260021cd95b314989c48e38aa053
Samir <samir@example.com>
2025-08-27 18:18:07 +0300

    add tooltip

--- src/widgets/AboutMe/ui/AboutMe.tsx
+++ src/widgets/AboutMe/ui/AboutMe.tsx
@@ -1,7 +1,8 @@
-import { Fragment } from "react";
 import { useRefsContext } from "../../../shared/contexts/RefsContext";
 import { H2Title } from "../../../shared/H2Title/H2Title";
 import { FormattedMessage } from "react-intl";
+import { Tooltip } from "antd";
+import { Fragment } from "react";
 
 const eventsMeta = ["2019", "2020", "2021", "2022", "2024"];
 
@@ -26,11 +27,32 @@ const AboutMe = () => {
           <div className="absolute w-[90%] bg-white h-3 rounded-md"></div>
           {events.map((event, index) => (
             <Fragment key={index}>
-              <p className="text-white text-[2.25rem] self-end">{event}</p>
-              <div className="w-[40px] h-[40px] rounded-[50%] bg-white"></div>
-              <p className="text-white text-[1.75rem] self-start">
-                <FormattedMessage id={`landing_event${event}`} />
-              </p>
+              <Tooltip
+                key={index}
+                title={
+                  <FormattedMessage id={`landing_event${event}_tooltip`} />
+                }
+              >
+                <p className="text-white text-[2.25rem] self-end">{event}</p>
+              </Tooltip>
+              <Tooltip
+                key={index}
+                title={
+                  <FormattedMessage id={`landing_event${event}_tooltip`} />
+                }
+              >
+                <div className="w-[40px] h-[40px] rounded-[50%] bg-white z-[2]"></div>
+              </Tooltip>
+              <Tooltip
+                key={index}
+                title={
+                  <FormattedMessage id={`landing_event${event}_tooltip`} />
+                }
+              >
+                <p className="text-white text-[1.75rem] self-start">
+                  <FormattedMessage id={`landing_event${event}`} />
+                </p>
+              </Tooltip>
             </Fragment>
           ))}
         </div>
